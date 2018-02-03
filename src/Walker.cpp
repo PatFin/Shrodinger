65c2deaa1d3e345f20704c47f53604d7e8107739
Patrick Finnerty <patrick.finnerty@insa-lyon.fr>
2018-02-03 20:40:01 +0900

    implemented functions Walk and B. Also created a default constructor which initializes the walker at random

--- src/Walker.cpp
+++ src/Walker.cpp
@@ -1,23 +1,58 @@
 #include <chrono>
+#include <math.h>
 #include <random>
 
 #include "Walker.h"
 
 using namespace std;
 
+double Walker::newRadius()
+{
+  return sqrt(x*x + y*y + z*z);
+}
+
+double Walker::oldRadius()
+{
+  return sqrt(pX*pX + pY*pY + pZ*pZ);
+}
 
 void Walker::Walk()
 {
-  //TODO
+  extern double tau;  
+  unsigned seed = std::chrono::system_clock::now().time_since_epoch().count();
+  std::default_random_engine generator (seed);
+  std::normal_distribution<double> distribution (0.0,1.0);
+
+  pX = x;
+  pY = y;
+  pZ = z;
+
+  x += sqrt(2*tau)*distribution(generator);
+  y += sqrt(2*tau)*distribution(generator);
+  z += sqrt(2*tau)*distribution(generator);
 }
 
 double Walker::B()
 {
-  //TODO
-  return 2.0;
+  extern double tau;  
+  double newV = 1/newRadius();
+  double oldV = 1/oldRadius();
+
+  return exp(tau*(newV - oldV)/2);
+}
+
+Walker::Walker()
+{
+  std::random_device rd;
+  std::mt19937 e2(rd());
+  std::uniform_real_distribution<> dist(0, 1);
+
+  x = dist(e2);
+  y = dist(e2);
+  z = dist(e2);
 }
 
 Walker::Walker(double initX, double initY, double initZ) :
-    x(initX), y(initY), z(initZ) {};
+  x(initX), y(initY), z(initZ) {}
 
 Walker::~Walker(){}
--- src/Walker.h
+++ src/Walker.h
@@ -1,14 +1,18 @@
 #pragma once
 
-class Walker
-{
-    private :
-        double x,y,z;
+class Walker{
+ private :
+  double x,y,z;//Coordinates of our walker
+  double pX, pY, pZ;//Coordinates of our walker before the last Walk
 
-    public :
-        void Walk();
-	double B();
+  double newRadius();
+  double oldRadius();
+  
+ public :
+  void Walk();
+  double B();
 	
-        Walker(double initX, double initY, double initZ);
-        virtual ~Walker();
+  Walker(double initX, double initY, double initZ);
+  Walker();
+  virtual ~Walker();
 };
